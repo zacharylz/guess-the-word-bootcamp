02cd972e2d491efddcbd3c365ddee7a8b9f05d3a
neokaiyuan <kai@rocketacademy.co>
2022-05-03 20:44:38 +0800

    Create starter code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React from "react";
-import logo from "./logo.png";
+import { getRandomWord } from "./utils.js";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    // Always call super with props in constructor to initialise parent class
+    super(props);
+    this.state = {
+      // currWord is the current secret word for this round. Update this with this.setState after each round.
+      currWord: getRandomWord(),
+      // guessedLetters stores all letters a user has guessed so far
+      guessedLetters: [],
+      // Insert num guesses left state here
+      // Insert form input state here
+    };
+    // This binding is necessary to make `this` work in the callback
+    this.generateWordDisplay = this.generateWordDisplay.bind(this);
+    // Insert binding of handleChange and handleSubmit here
+  }
+
+  generateWordDisplay() {
+    const wordDisplay = [];
+    // for...of is a string and array iterator that does not use index
+    for (let letter of this.state.currWord) {
+      if (this.state.guessedLetters.includes(letter)) {
+        wordDisplay.push(letter);
+      } else {
+        wordDisplay.push("_");
+      }
+    }
+    return wordDisplay.toString();
+  }
+
+  // Insert form callback functions handleChange and handleSubmit here
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>Guess The Word 🚀</h1>
+          <h3>Word Display</h3>
+          {this.generateWordDisplay()}
+          <h3>Guessed Letters</h3>
+          {this.state.guessedLetters.length > 0
+            ? this.state.guessedLetters.toString()
+            : "-"}
+          <h3>Input</h3>
+          {/* Insert form element here */}
+          Todo: Insert form element here
         </header>
       </div>
     );
